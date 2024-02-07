b4519fe4613fa7accfead5873f4fa3673906e506
Aryan Pandey <margiaryan@gmail.com>
2024-02-07 16:35:36 +0530

    storing z-values and co-ordinate

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,30 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 fn reading_file() {
-    let mut z_value = 0;
-    let mut x = 0;
-    let mut y = 0;
-    let mut z = 0;
+    let mut z_value = Vec::<i32>::new();
+    let mut x = Vec::<f32>::new();
+    let mut y = Vec::<f32>::new();
+    let mut z = Vec::<f32>::new();
+
     let data = File::open("sample.dat").unwrap_or_else(|error| panic!("ERROR: {}", error));
-    let mut buf_reader = BufReader::new(data);
-    for l in buf_reader.lines() {
-        l.unwrap();
+    let buf_reader = BufReader::new(data);
+    let mut lines = buf_reader.lines();
+    lines.next();
+
+    for l in lines {
+        if let Ok(ip) = l {
+            let vec = ip.split_whitespace().collect::<Vec<&str>>();
+            z_value.push(vec[0].parse::<i32>().unwrap());
+            x.push(vec[1].parse::<f32>().unwrap());
+            y.push(vec[2].parse::<f32>().unwrap());
+            z.push(vec[3].parse::<f32>().unwrap());
+        }
     }
+
+    println!("{:?}", z_value);
+    println!("{:?}", x);
+    println!("{:?}", y);
+    println!("{:?}", z);
 }
 
 fn main() {
